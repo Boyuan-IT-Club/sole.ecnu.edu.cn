46e620abaf09b6ff3c20ff85573d59baf8ad3f20
suiko_74 <10234304420@stu.ecnu.edu.cn>
2025-07-29 08:47:11 +0800

    fix: img.js

--- src/js/img.js
+++ src/js/img.js
@@ -1,26 +1,28 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   const imgs = document.querySelectorAll('.article img');
+document.addEventListener('DOMContentLoaded', function () {
+  const imgs = document.querySelectorAll('.article img');
+  const isMobile = window.innerWidth <= 768;
+  if (isMobile) {
+    imgs.forEach(img => {
+    // 清除 img 的 style 属性
+    img.removeAttribute('style');
 
-//   imgs.forEach(img => {
-//     // 清除 img 的 style 属性
-//     img.removeAttribute('style');
+    const parent = img.parentElement;
 
-//     const parent = img.parentElement;
+    // 情况 1：img 的父元素是 <p> 且 p 只有这一个子元素
+    if (parent && parent.tagName === 'P' && parent.children.length === 1) {
+      parent.removeAttribute('style');
+    }
 
-//     // 情况 1：img 的父元素是 <p> 且 p 只有这一个子元素
-//     if (parent && parent.tagName === 'P' && parent.children.length === 1) {
-//       parent.removeAttribute('style');
-//     }
-
-//     // 情况 2：img 的父元素是 <span>，span 的父元素是 <p>，且 p 只有这一个子元素（span）
-//     if (
-//       parent &&
-//       parent.tagName === 'SPAN' &&
-//       parent.parentElement &&
-//       parent.parentElement.tagName === 'P' &&
-//       parent.parentElement.children.length === 1
-//     ) {
-//       parent.parentElement.removeAttribute('style');
-//     }
-//   });
-// });
+    // 情况 2：img 的父元素是 <span>，span 的父元素是 <p>，且 p 只有这一个子元素（span）
+    if (
+      parent &&
+      parent.tagName === 'SPAN' &&
+      parent.parentElement &&
+      parent.parentElement.tagName === 'P' &&
+      parent.parentElement.children.length === 1
+    ) {
+      parent.parentElement.removeAttribute('style');
+    }
+  });
+  }
+});
