eeed2b3872a31fea335efb59548e5b0f6eec4374
lllzh <lzh050206@gmail.com>
2025-09-05 00:34:52 +0800

    feat: banner swiper

--- src/js/banner.js
+++ src/js/banner.js
@@ -441,3 +441,24 @@ document.addEventListener('DOMContentLoaded', function () {
     //     e.preventDefault();
     // });
 });
+
+
+
+$(function () {
+    var swiper = new Swiper(".swiper-banner", {
+      loop: true,
+      speed: 1000,
+      autoplay: {
+        delay: 4000,
+        stopOnLastSlide: false,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  });
